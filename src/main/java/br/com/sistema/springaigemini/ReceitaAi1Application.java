e6da20b1ff6918689d588ac166fc0e90c1dcceb2
Cesar Augusto <cesar.augusto.rj1@gmail.com>
2026-01-20 10:50:59 -0300

    first commit

---
+++ src/main/java/br/com/sistema/springaigemini/ReceitaAi1Application.java
@@ -0,0 +1,13 @@
+package br.com.sistema.springaigemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceitaAi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceitaAi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sistema/springaigemini/ReceitaAi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistema.springaigemini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitaAi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
